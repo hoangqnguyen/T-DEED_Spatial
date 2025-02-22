36ec9b2ff1b5ce4b306bc8988cec35b6419d5899
hoang@r48.kist <hoang@r48.kist>
2025-02-22 16:32:37 +0900

    added vnl1.5 and can run on store mode. TODO: try training without sptial head first, the add the spatial head and train later

--- util/dataset.py
+++ util/dataset.py
@@ -4,19 +4,21 @@ from util.io import load_text
 
 
 DATASETS = [
-    'tennis',
-    'fs_perf',
-    'fs_comp',
-    'finediving',
-    'finegym',
-    'soccernetv2',
-    'soccernetball'
+    "tennis",
+    "fs_perf",
+    "fs_comp",
+    "finediving",
+    "finegym",
+    "soccernetv2",
+    "soccernetball",
+    "vnl_1.5",
 ]
 
 
 def load_classes(file_name):
     return {x: i + 1 for i, x in enumerate(load_text(file_name))}
 
+
 def read_fps(video_frame_dir):
-    with open(os.path.join(video_frame_dir, 'fps.txt')) as fp:
+    with open(os.path.join(video_frame_dir, "fps.txt")) as fp:
         return float(fp.read())
